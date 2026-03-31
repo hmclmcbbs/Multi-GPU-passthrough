a8804b3df444415fcfb8769d4724cede79446b8d
Folk15305 <91120223+hmclmcbbs@users.noreply.github.com>
2026-03-31 12:41:46 +0800

    Update

--- dgpu-connect.sh
+++ dgpu-connect.sh
@@ -2,4 +2,6 @@
 sudo systemctl stop lactd.service
 sudo modprobe -r vfio-pci
 sudo modprobe nvidia_drm nvidia_modeset nvidia_uvm nvidia
+sudo systemctl reset-failed nvidia-powerd.service
 sudo systemctl restart nvidia-persistenced nvidia-powerd.service lactd.service
+echo "Finished"
--- dgpu-disconnect.sh
+++ dgpu-disconnect.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+sudo fuser -k -9 /dev/nvidia*
+sudo fuser -k -9 /dev/dri/renderD129
+sudo fuser -k -9 /dev/dri/renderD129
+sudo fuser -k -9 /dev/dri/renderD129
+sudo fuser -k -9 /dev/dri/card0
+sudo fuser -k -9 /dev/dri/card0
+sudo fuser -k -9 /dev/dri/card0
 sudo systemctl stop nvidia-persistenced nvidia-powerd.service lactd.service
-sudo rmmod -f nvidia_drm nvidia_modeset nvidia_uvm nvidia
+sudo rmmod nvidia_drm nvidia_modeset nvidia_uvm nvidia
 sudo modprobe vfio-pci ids=10de:28a0,10de:22be
 sudo systemctl restart lactd.service
+echo "Finished"
